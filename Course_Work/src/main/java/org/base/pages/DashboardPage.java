5cee2c7d3090af03483181833aa4780c75f46dfa
OSZB <zaremba.olesia.i@gmail.com>
2023-10-19 17:40:23 +0300

    add coursetask

--- Course_Work/src/main/java/org/base/pages/DashboardPage.java
+++ Course_Work/src/main/java/org/base/pages/DashboardPage.java
@@ -11,8 +11,8 @@ public class DashboardPage
     public SelenideElement overview  = Selenide.$x("//*[@id=\"dashboard\"]/div[1]/ul/li[1]/a[contains()@href, 'dashboard/1')]");
 
     private SelenideElement newProjectBtn = Selenide.$x("//section//a[@href='/project/create']");
-    private SelenideElement nameProjectForm = $x("//div[@id=\\\"modal-box\\\"]\"name");
-    private SelenideElement idProjectForm = $x("//div[@id=\\\"modal-box\\\"]\"identifier");
+    private SelenideElement nameProjectForm = $x("//div[@id=\"modal-box\"]\"name");
+    private SelenideElement idProjectForm = $x("//div[@id=\"modal-box\"]\"identifier");
     private SelenideElement saveProjectButton = $x("//div[@class=\"form-actions\"]/button");
 
     public void createProject(String name, String id) {
